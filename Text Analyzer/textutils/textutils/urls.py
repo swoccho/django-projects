5568d4266de560a1e00ca0f7b744c7c05e4e22be
swoccho <asikurrs09@gmail.com>
2022-04-22 11:51:34 +0600

    bootstrap added

--- Text Analyzer/textutils/textutils/urls.py
+++ Text Analyzer/textutils/textutils/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -20,6 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index , name= 'index'),
-    path('analyze' , views.analyze, name = 'analyze')
+    path('analyze' , views.analyze, name = 'analyze'),
+    path('about' , views.about , name= 'about') 
 
 ]
--- Text Analyzer/textutils/textutils/views.py
+++ Text Analyzer/textutils/textutils/views.py
@@ -1,19 +1,17 @@
 import re
 from string import punctuation
-from django.http import HttpResponse,Http404
 from django.shortcuts import render
-
-
 
 def index(request):
     return render(request , 'index.html')
 
-def _analization(purpose,analyzed):
-    return {'purpose': purpose, 'analyzed_text': analyzed}
+def about(request):
+    return render(request, 'about.html')
 
 
 def analyze(request):
     user_text = request.POST.get('text', 'default')
+    user_input = request.POST.get('text' , 'default')
     removepunc = request.POST.get('removepunc' , 'off')
     captalize = request.POST.get('uppercase' , 'off')
     remove_newline = request.POST.get('removenewline' , 'off')
@@ -27,18 +25,18 @@ def analyze(request):
             for char in user_text:
                 if char not in punctuation:
                     analyzed = analyzed +char
-
-            analyzation = {'purpose': 'Removed Punctuation' , 'analyzed_text' : analyzed}
             user_text = analyzed
+            analyzation = {'purpose': 'Removed Punctuation' ,'user_input': user_input, 'analyzed_text' : user_text}
+            
 
 
         if  captalize == 'on':
             analyzed =''
             for char in user_text:
                 analyzed = analyzed + char.upper()
-
-            analyzation = {'purpose': 'Captalized Text', 'analyzed_text': analyzed}
             user_text = analyzed
+            analyzation = {'purpose': 'Captalized Text','user_input': user_input, 'analyzed_text': user_text}
+            
 
         
         if remove_newline == 'on':
@@ -46,16 +44,32 @@ def analyze(request):
             for char in user_text:
                 if char != '\n' and char != '\r':
                     analyzed = analyzed + char
-            analyzation = _analization('New line removed' , analyzed)
+            user_text = analyzed
+
+            analyzation = {'purpose': 'Captalized Text','user_input': user_input, 'analyzed_text': user_text}
+
+
+
+            
         
 
         if removespace == 'on':
-            analyzed =''
-            for index ,char in enumerate(user_text):
-                if not (user_text[index] == ' ' and user_text[index +1] == ' '):
-                    analyzed = analyzed + char
-            analyzation =  _analization('Extra spaces Removed' , analyzed)
+            # analyzed =''
+            
+            # for index ,char in enumerate(user_text):
+            #     if not (user_text[index] == ' ' and user_text[index +1] == ' '):
+            #         analyzed = analyzed + char
+            #         
+          
+            def remove_extra_space():
+                words = [word for word in user_text.split(' ') if word != '']
+                return ' '.join(words)
+
+            analyzed = remove_extra_space()
+    
             user_text= analyzed
+            analyzation = {'purpose': 'Extra spaces Removed' ,'user_input':user_input, 'analyzed_text' : user_text}
+            
 
 
         if charcount == 'on':
@@ -63,14 +77,22 @@ def analyze(request):
             count = 0
             for char in  user_text:
                 count += 1
-            analyzation = _analization('CHaracter counted' , f'Your sentence has {count} characters')
+            analyzation = {'method' : 'character counted' ,'user_input':user_input,'analyzed_text':user_text, 'char_text' : f'Your sentence has {count} characters' }
             
 
         if wordcount == 'on':
             count = len(re.findall(r'\w+' , user_text))
 
-            analyzation = _analization('Word counted' , f' Your sentence has {count} words.')
+            analyzation = {'words':'Word counted' ,'user_input':user_input,'analyzed_text':user_text ,'word_text': f' Your sentence has {count} words.'}
 
+        if (wordcount=='on' and charcount == 'on'):
+            character_count = 0
+            for char in user_text:
+                character_count +=1
+
+            word_count = len(re.findall(r'\w+' , user_text))
+
+            analyzation = {'user_input' : user_input ,'analyzed_text':user_text, 'word_and_char_count' : f'Your sentence has {character_count} characters and {word_count} words.'}
 
         return render(request, 'analyzed.html',analyzation)
 
